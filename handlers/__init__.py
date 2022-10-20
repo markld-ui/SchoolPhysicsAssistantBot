4b7cf8d3e8bca9cc74d29febe4d1f718a00c4e39
Roman Slinkov <thankr3@gmail.com>
2022-10-20 23:40:00 +0500

    remove admin

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,4 +2,8 @@ from . import Start
 from . import AboutBot
 from . import LearnClient
 from . import HelperClient
+<<<<<<< HEAD
+=======
+from . import Admin
+>>>>>>> e7080c4 (2.0 version of bot)
 from . import State
