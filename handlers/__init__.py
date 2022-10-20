abafbe3bf3792d7cc5b7b564f900ed3b61974080
Roman Slinkov <thankr3@gmail.com>
2022-10-21 00:03:58 +0500

    del sm elem

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,8 +2,5 @@ from . import Start
 from . import AboutBot
 from . import LearnClient
 from . import HelperClient
-<<<<<<< HEAD
-=======
 from . import Admin
->>>>>>> e7080c4 (2.0 version of bot)
 from . import State
